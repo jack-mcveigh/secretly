4f1f2355663976af44219c217c755b3bec63de6f
Jack McVeigh <jmcveigh55@gmail.com>
2023-04-08 00:46:33 -0400

    Fix setMap; Implement setText, simplfy setMap
    setMap would always return the error for key not in map. Also, setting
    the value for setMap is the same as for setText after the value is
    parsed from the map. setMap now calls setText to dry it up

--- internal/field.go
+++ internal/field.go
@@ -176,7 +176,60 @@ func (f *Field) Set(b []byte) error {
 }
 
 func (f *Field) setText(b []byte) error {
-	return errors.New("not implemented")
+	const ErrFailedConvertFormat = "failed to convert secret \"%s's\" key, \"%s\" to %s: %w"
+
+	value := string(b)
+
+	typ := f.Value.Type()
+	switch f.Value.Kind() {
+	case reflect.String:
+		f.Value.SetString(value)
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		var (
+			v   int64
+			err error
+		)
+
+		if f.Value.Kind() == reflect.Int64 && typ.PkgPath() == "time" && typ.Name() == "Duration" {
+			var d time.Duration
+			d, err = time.ParseDuration(value)
+			v = int64(d)
+		} else {
+			v, err = strconv.ParseInt(value, 0, typ.Bits())
+		}
+		if err != nil {
+			t := fmt.Sprintf("int%d", typ.Bits())
+			return fmt.Errorf(ErrFailedConvertFormat, f.SecretName, f.MapKeyName, t, err)
+		}
+
+		f.Value.SetInt(v)
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		v, err := strconv.ParseUint(value, 0, typ.Bits())
+		if err != nil {
+			t := fmt.Sprintf("uint%d", typ.Bits())
+			return fmt.Errorf(ErrFailedConvertFormat, f.SecretName, f.MapKeyName, t, err)
+		}
+		f.Value.SetUint(v)
+
+	case reflect.Bool:
+		v, err := strconv.ParseBool(value)
+		if err != nil {
+			return fmt.Errorf(ErrFailedConvertFormat, f.SecretName, f.MapKeyName, "bool", err)
+		}
+		f.Value.SetBool(v)
+
+	case reflect.Float32, reflect.Float64:
+		v, err := strconv.ParseFloat(value, typ.Bits())
+		if err != nil {
+			t := fmt.Sprintf("float%d", typ.Bits())
+			return fmt.Errorf(ErrFailedConvertFormat, f.SecretName, f.MapKeyName, t, err)
+		}
+		f.Value.SetFloat(v)
+	}
+
+	return nil
 }
 
 func (f *Field) setMap(b []byte) error {
@@ -199,57 +252,8 @@ func (f *Field) setMap(b []byte) error {
 	return errors.New("secret is not a valid map")
 
 readSecretMap:
-	const convertErrFormat = "failed to convert secret \"%s's\" key, \"%s\" to %s: %w"
-
 	if value, ok := secretMap[f.MapKeyName]; ok {
-		typ := f.Value.Type()
-		switch f.Value.Kind() {
-		case reflect.String:
-			f.Value.SetString(value)
-
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			var (
-				v   int64
-				err error
-			)
-
-			if f.Value.Kind() == reflect.Int64 && typ.PkgPath() == "time" && typ.Name() == "Duration" {
-				var d time.Duration
-				d, err = time.ParseDuration(value)
-				v = int64(d)
-			} else {
-				v, err = strconv.ParseInt(value, 0, typ.Bits())
-			}
-			if err != nil {
-				t := fmt.Sprintf("int%d", typ.Bits())
-				return fmt.Errorf(convertErrFormat, f.SecretName, f.MapKeyName, t, err)
-			}
-
-			f.Value.SetInt(v)
-
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			v, err := strconv.ParseUint(value, 0, typ.Bits())
-			if err != nil {
-				t := fmt.Sprintf("uint%d", typ.Bits())
-				return fmt.Errorf(convertErrFormat, f.SecretName, f.MapKeyName, t, err)
-			}
-			f.Value.SetUint(v)
-
-		case reflect.Bool:
-			v, err := strconv.ParseBool(value)
-			if err != nil {
-				return fmt.Errorf(convertErrFormat, f.SecretName, f.MapKeyName, "bool", err)
-			}
-			f.Value.SetBool(v)
-
-		case reflect.Float32, reflect.Float64:
-			v, err := strconv.ParseFloat(value, typ.Bits())
-			if err != nil {
-				t := fmt.Sprintf("float%d", typ.Bits())
-				return fmt.Errorf(convertErrFormat, f.SecretName, f.MapKeyName, t, err)
-			}
-			f.Value.SetFloat(v)
-		}
+		return f.setText([]byte(value))
 	}
 
 	return fmt.Errorf("the secret map, \"%s\" does not contain key \"%s\"", f.SecretName, f.MapKeyName)
