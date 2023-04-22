5a6e9d63775dd22b640d49b41ddeeaef49803314
Jack McVeigh <jmcveigh55@gmail.com>
2023-04-22 15:27:50 -0400

    Add secretly.go to contain pkg doc

--- client.go
+++ client.go
@@ -1,8 +1,3 @@
-// Package secretly provides a client wrapper
-// for popular secret management services.
-//
-// The secretly package's client interface exposes convenience methods
-// for retrieving secrets.
 package secretly
 
 import (
---
+++ secretly.go
@@ -0,0 +1,6 @@
+// Package secretly provides a client wrapper
+// for popular secret management services.
+//
+// The secretly package's client interface exposes convenience methods
+// for retrieving secrets.
+package secretly
